888000983d177977448e156a8c207b876aab6534
Simone Vidotti <vidottisimone@protonmail.com>
2024-05-18 16:55:13 +0200

    Update Sorgente.ino

--- Index/Sorgente/Sorgente.ino
+++ Index/Sorgente/Sorgente.ino
@@ -1,6 +1,6 @@
 // Vidotti Simone, Zhou Luigi 4 TEL A Progetto LineFollower, Robottino Arduino
-#include <Servo.h>
-#include <IRremote.hpp>
+#include <IRremote.hpp> // Libreria per telecomando IR
+#include <Servo.h> // Libreria per gestire i servo-motori
 
 // pin Sensori
 #define SensoreR A2
@@ -12,19 +12,18 @@
 #define trigPin 2 // pin D3 Arduino Trig dell'HC-SR04
 
 // pin ricevitore
-#define IR_RECEIVE_PIN 8
+#define IR_RECEIVE_PIN 12
 
 // pin Servo-motori
 Servo ServoR;
 Servo ServoL;
 
 // Velocita per fixing line
-int DefaultR = 1491;
-int DefaultL = 1509;
+int DefaultR = 1460;
+int DefaultL = 1540;
 
-// Soglie, Sensore centrale ha una soglia diversa
-int Soglia1 = 1020;
-int Soglia2 = 1000;
+// Soglia sensori
+int Soglia = 1000;
 
 // Valori di lettura
 int LetturaR, LetturaC, LetturaL;
@@ -39,77 +38,110 @@ bool start = false; // Controllo quando partire
 // Controllo Ostacolo, guida manuale.
 bool Superamento = false; // Ctrl superamento
 
+// Controllo partenza
+Ctrl_Start() {
+    if (IrReceiver.decode()) {
+        uint16_t command = IrReceiver.decodedIRData.command;
+
+        if (command == 69) {
+            start = true;
+        }
+
+    }
+
+    IrReceiver.resume();
+}
+
 // --- Seguire Linea nera ---
-void Lettura() {
+void Lettura() { //Lettura sensori IR
     LetturaR = analogRead(SensoreR);
     LetturaC = analogRead(SensoreC);
     LetturaL = analogRead(SensoreL);
 }
 
-void Avanti() {
-    ServoR.writeMicroseconds(1470);
-    ServoL.writeMicroseconds(1530);
+void Avanti() { // Andare avanti
+    ServoR.writeMicroseconds(1450);
+    ServoL.writeMicroseconds(1550);
 }
 
-void LeggermenteDestra() {
+void LeggermenteDestra() { // Svoltare a destra
     ServoR.writeMicroseconds(1500);
     ServoL.writeMicroseconds(1530);
     delay(500);
 }
 
-void LeggermenteSinistra() {
+void LeggermenteSinistra() { // Svoltare a sinistra
     ServoR.writeMicroseconds(1470);
     ServoL.writeMicroseconds(1500);
     delay(500);
 }
 
-void Fermo() {
-    
+void Fermo() { // Stato di quiete, il robot sta fermo
     ServoR.writeMicroseconds(1500);
     ServoL.writeMicroseconds(1500);
 }
 
-void Stop() {
+void Stop() { // Stop quando arriva al traguardo
     ServoR.writeMicroseconds(1500);
     ServoL.writeMicroseconds(1500);
     ServoR.detach();
     ServoL.detach();
-    digitalWrite(7, HIGH);
+    start = false;
 }
 
+void Traguardo() {
+    // Allarme per circa 8s e dopo led accesso quando è arrivato al Traguardo
+    for (int i=0; i < 5; i++) {
+        tone(6, 3000); // frequenza
+        delay(200);        //
+        digitalWrite(7, HIGH);
+        delay(200);        //
+
+        noTone(6);     // buzzer off
+        delay(100);
+        digitalWrite(7, LOW);
+    }
+
+    while (start == false) {
+        noTone(6);     // buzzer off
+
+        digitalWrite(7, HIGH);
+    }
+}
+
+// --- Codifica lettura soglie --- distinguere nero e bianco
 void Codifica() {
     //Casi Definiti
-
-    while(LetturaL >= Soglia1 && LetturaC >= Soglia2 && LetturaR < Soglia1) {
+    while(LetturaL >= Soglia && LetturaC >= Soglia && LetturaR < Soglia) {
         LeggermenteSinistra();
         Lettura();
     }
 
-    while(LetturaL >= Soglia1 && LetturaC < Soglia2 && LetturaR < Soglia1) {
+    while(LetturaL >= Soglia && LetturaC < Soglia && LetturaR < Soglia) {
         LeggermenteSinistra();
         Lettura();
     }
 
-    while(LetturaL < Soglia1 && LetturaC >= Soglia2 && LetturaR >= Soglia1) {
+    while(LetturaL < Soglia && LetturaC >= Soglia && LetturaR >= Soglia) {
         LeggermenteDestra();
         Lettura();
     }
 
-    while(LetturaL < Soglia1 && LetturaC >= Soglia2 && LetturaR < Soglia1) {
+    while(LetturaL < Soglia && LetturaC >= Soglia && LetturaR < Soglia) {
         Avanti();
         Lettura();
     }
 
-    while(LetturaL < Soglia1 && LetturaC < Soglia2 && LetturaR >= Soglia1) {
+    while(LetturaL < Soglia && LetturaC < Soglia && LetturaR >= Soglia) {
         LeggermenteDestra();
         Lettura();
     }
 
-    if (LetturaL >= Soglia1 && LetturaC >= Soglia2 && LetturaR >= Soglia1) {
+    if (LetturaL >= Soglia && LetturaC >= Soglia && LetturaR >= Soglia) {
         Stop();
     }
 
-    if (LetturaL < Soglia1 && LetturaC < Soglia2 && LetturaR < Soglia1) {
+    if (LetturaL < Soglia && LetturaC < Soglia && LetturaR < Soglia) {
             ServoR.writeMicroseconds(1500);
             ServoL.writeMicroseconds(1500);
     }
@@ -119,27 +151,19 @@ void Codifica() {
     ServoL.writeMicroseconds(DefaultL);
 }
 
-// Rivelamento ricevitore IR per Telecomando
+// --- Rivelamento ricevitore IR per Telecomando ---
 void Ctrl_Telecomando() {
     if (IrReceiver.decode()) {
-        uint16_t command = IrReceiver.decodedIRData.command;
-
-        if (command == 69) {
-            start = true;
-        }
-
         if (command == 24) {
             ServoR.writeMicroseconds(1400);
             ServoL.writeMicroseconds(1600);
-            delay(300);
         } else if (command == 82) {
             ServoR.writeMicroseconds(1600);
             ServoL.writeMicroseconds(1400);
-            delay(300);
         } else if (command == 90) {
             ServoR.writeMicroseconds(1500);
             ServoL.writeMicroseconds(1600);
-            delay(500);
+
         } else if (command == 8) {
             ServoR.writeMicroseconds(1400);
             ServoL.writeMicroseconds(1500);
@@ -147,9 +171,62 @@ void Ctrl_Telecomando() {
         } else if (command == 70){
           Superamento = false;
         }
+    } else {
+        Fermo();
     }
     
     IrReceiver.resume();
+}
+
+// --- Supera Ostacolo ---
+void SuperaOstacolo() {
+   // Leggermente indietro
+   ServoR.writeMicroseconds(1550);
+   ServoL.writeMicroseconds(1450);
+   delay(300);
+
+    // Destra su se stesso
+   ServoR.writeMicroseconds(1550);
+   ServoL.writeMicroseconds(1550);
+   delay(700);
+
+   // Avanti
+   Avanti();
+   delay(1600);
+
+   // Sinistra su se stesso
+   ServoR.writeMicroseconds(1450);
+   ServoL.writeMicroseconds(1450);
+   delay(700);
+
+   // Avanti
+   Avanti();
+   delay(1600);
+
+   // Sinistra su se stesso
+   ServoR.writeMicroseconds(1450);
+   ServoL.writeMicroseconds(1450);
+   delay(450);
+
+   bool Linea = false;
+
+   while(Linea == false) {
+    Lettura();
+
+    //Default
+    ServoR.writeMicroseconds(DefaultR);
+    ServoL.writeMicroseconds(DefaultL);
+
+    if (LetturaC >= Soglia) { // Controllo rivelamento linea sensore centrale
+      ServoR.writeMicroseconds(1550);
+      ServoL.writeMicroseconds(1550);
+      delay(700);
+
+      Linea = true;
+
+      break;
+    }
+   }
 }
 
 // --- Controllo Ostacolo ---
@@ -163,14 +240,14 @@ void Ctrl_Ostacolo() {
     t = pulseIn(echoPin, HIGH); // tempo di ritorno del segnale
     d = t * 0.0344 / 2; //  distanza in cm
 
-    if (d <= 10 || d <= 5) {
+    if (d <= 10) {
         Fermo();
 
         Superamento = true;
 
-        for (int i=0; i < 3; i++) {
-            // Suona il buzzer per 500 ms
-            tone(6, 3000); // Send 1KHz sound signal...
+        for (int i=0; i < 5; i++) {
+            // Suona il buzzer per circa 8s
+            tone(6, 3000); // 
             delay(200);        //
             digitalWrite(7, HIGH);
             delay(200);        //
@@ -182,18 +259,19 @@ void Ctrl_Ostacolo() {
 
         noTone(6);     // Stop sound...
 
-        //Lampeggio led mentre è telecomandato
-        while (Superamento) {
-            digitalWrite(7, HIGH);
-            delay(50);
-            digitalWrite(7, LOW);
-            delay(50);
+        digitalWrite(7, HIGH);
 
+        /*
+          // Dovrebbe andare in guida manuale, ma non va:
+          while(Superamento) {
             Ctrl_Telecomando();
-        }
-    } 
+          }
+        */
+      
+        SuperaOstacolo();
 
-    Superamento = false;
+        // Superamento = false;
+    } 
 }
 
 //Setup
@@ -208,7 +286,7 @@ void setup() {
 
     // Setup Ricevitore
     IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-    pinMode(IR_RECEIVE_PIN, INPUT);
+    //pinMode(IR_RECEIVE_PIN, INPUT);
 
     // Setup Servo-motori
     ServoR.attach(10);
@@ -217,11 +295,13 @@ void setup() {
 
 //main loop
 void loop() {
- Ctrl_Telecomando();
+ Ctrl_Start();
 
  while (start) {
      Ctrl_Ostacolo();
      Lettura();
      Codifica();
  }
+
+ Traguardo();
 }
